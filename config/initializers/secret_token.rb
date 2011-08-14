bb439b0eb0c5cc132f415322feebf1d95ffe3ab5
atul <aatul.thapliyal@gmail.com>
2011-08-13 19:26:34 -0600

    Commiting static pages

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Website::Application.config.secret_token = '629c6b54d032394514d49d03df541f963005acae1fde88502146c5662114c6562e994f7ca2ef28789f71c37adf8d7b8e3dda5b56b2fcf5cd46597de2e032a39a'
