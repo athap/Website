e36d274686aaea00a3988e8bfa3937f766b482e7
atul <aatul.thapliyal@gmail.com>
2011-08-22 15:01:41 -0600

    Done with the initial layout

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,4 +6,8 @@ class PagesController < ApplicationController
   def contact
     @title = "Contact"
   end
+
+  def about
+    @title = "About"
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def title
+    default_title = "Rails App"
+    if @title.nil?
+      default_title
+    else
+      @title
+    end
+  end
 end
